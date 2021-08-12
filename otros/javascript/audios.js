a9e0d1dc7e21605cc4a615d6e4c827692dbe6fa9
JohanAnim <gutierrezjohanantonio@gmail.com>
2021-08-12 14:24:35 -0500

    Pocos cambios

--- otros/javascript/audios.js
+++ otros/javascript/audios.js
@@ -2,14 +2,13 @@ var menuprincipal = document.querySelector("#menuprincipal");
 let audioEtiqueta = document.querySelector("audio")
 
 function menuaudio() {
-
 if (menuprincipal.ariaExpanded == "false") {
-menuprincipal.innerHTML="Menú principal. (Abierto)";
-
+  menuprincipal.innerHTML="Menú principal. (Abierto)";
+  
       audioEtiqueta.setAttribute("src", "audio/audex/audio.mp3")
       audioEtiqueta.play()
       console.log(`Reproduciendo: ${audioEtiqueta.src}`)
-      document.querySelector("#homeFocus").focus();
+    
     }
 
   else {  
@@ -18,9 +17,17 @@ menuprincipal.innerHTML="Menú principal. (Abierto)";
     audioEtiqueta .setAttribute("src", "audio/audex/audio1.mp3")
     audioEtiqueta .play()
       console.log(`Reproduciendo: ${audioEtiqueta .src}`)
-    
   }
-
+  
 }
+  
+//Función para establecer el foco al elemento "dni"
+
+const setFocusDNI = (event) => {
+  $('.homeFocus').focus();
+}
+
+// Añadimos nuestra función para manejar el evento del elemento tipo `Collapse`
+$('.collapse').on('shown.bs.collapse', setFocusDNI);
 
 menuprincipal.addEventListener("click", menuaudio);
